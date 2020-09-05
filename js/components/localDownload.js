ef7a10b8d309418a79ff89887c9f4abbafeab136
TheRolf <yann.lv@live.fr>
2020-09-05 18:20:07 +0200

    !

--- js/components/localDownload.js
+++ js/components/localDownload.js
@@ -3,7 +3,7 @@ Vue.component('local-download', {
     canPack: Boolean,
   },
   template: 
-    '<button id="DownloadLocally" :disabled="!canPack && !isDownloading" class="btn btn-block" v-on:click="downloadLocally">Download locally (ALPHA)</button>',
+    '<button id="DownloadLocally" :disabled="!canPack && isDownloading" class="btn btn-block" v-on:click="downloadLocally">Download locally (ALPHA)</button>',
   data() {
     return {
       dbName: 'faithful',
